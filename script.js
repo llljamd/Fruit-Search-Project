f787cbcd7c8f9fd03dd1456fe85ad50a0acba202
llljamd <llljamdan@gmail.com>
2023-07-04 00:23:58 -0600

    implemented showSuggestions, updated searchHandler() and search()
    Condensed the search() function.
    
    Condensed the searchHandler() function.
    
    Implemented a list that updates per users input.

--- script.js
+++ script.js
@@ -4,27 +4,33 @@ const suggestions = document.querySelector('.suggestions ul');
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
 function search(str) {
-	let results = [];
 
-	// TODO
-	results = fruit.map(element => {
-		return element.toLowerCase()}).filter(suggestions => {
-		return suggestions.includes(str);
-		})
-
-	console.log(results);
-	return results;
+	return fruit.filter(suggest => {
+		if(suggest.toLowerCase().includes(str)) {
+			return suggest
+		}})
 }
 
 function searchHandler(e) {
-	// TODO
-	let str = e.target.value.toLowerCase();
-	search(str);
+	
+	const options = search(e.target.value.toLowerCase());
+	showSuggestions(options, e.target.value.toLowerCase())
 }
 
 function showSuggestions(results, inputVal) {
 
-	// TODO
+	suggestions.replaceChildren();
+
+	if(inputVal.length !== 0) {
+		results.forEach(element => {
+			const newItem = document.createElement("li");
+			const index = element.toLowerCase().indexOf(inputVal);
+	
+			newItem.innerHTML = element.substr(0, index) + "<b>" + element.substr(index, inputVal.length) + "</b>" + element.substr(inputVal.length + index);
+	
+			suggestions.appendChild(newItem);
+		});
+	}
 }
 
 function useSuggestion(e) {
